108abd197c3b55958a9a7d5af70338e40d96c0ab
Kenneth Nguyen <n.kennethcs@gmail.com>
2021-04-25 12:27:03 -0700

    add replies view to landscape comments

--- src/features/post/Comment.js
+++ src/features/post/Comment.js
@@ -16,7 +16,7 @@ import Button from "@material-ui/core/Button";
 /* A Comment component */
 /* Only show the avatar and subheaders (time elapseds since commented, likes, and reply button) on landscape view */
 const Comment = ({ comment, isLandscape }) => {
-  const { postedBy, message, liked, id, profilePicture } = comment;
+  const { postedBy, message, liked, id, profilePicture, replies } = comment;
   const dispatch = useDispatch();
   return (
     <ListItem>
@@ -41,23 +41,40 @@ const Comment = ({ comment, isLandscape }) => {
         }
         secondary={
           isLandscape && (
-            <Box width={300} display="flex">
-              <Typography>11h &nbsp;</Typography>
-              {liked && (
-                <Typography style={{ color: "grey" }}>1 like &nbsp;</Typography>
+            <Box width={300} display="flex" flexDirection="column">
+              <Box width={300} display="flex" style={{ gap: 12 }}>
+                <Typography>11h</Typography>
+                {liked && (
+                  <Typography style={{ color: "grey" }}>1 like</Typography>
+                )}
+                <Button
+                  disableRipple
+                  style={{
+                    color: "grey",
+                    textTransform: "none",
+                    backgroundColor: "transparent",
+                    padding: "1px 0 0 0",
+                    minWidth: 0,
+                  }}
+                >
+                  Reply
+                </Button>
+              </Box>
+              {replies.length > 0 && (
+                <Box width={300} display="flex">
+                  <div
+                    style={{
+                      color: "grey",
+                      margin: "2px 10px 0px 10px",
+                    }}
+                  >
+                    ―
+                  </div>
+                  <Typography style={{ color: "grey" }}>
+                    View replies ({replies.length})
+                  </Typography>
+                </Box>
               )}
-              <Button
-                disableRipple
-                style={{
-                  color: "grey",
-                  textTransform: "none",
-                  backgroundColor: "transparent",
-                  padding: "1px 0 0 0",
-                  minWidth: 0,
-                }}
-              >
-                Reply
-              </Button>
             </Box>
           )
         }
